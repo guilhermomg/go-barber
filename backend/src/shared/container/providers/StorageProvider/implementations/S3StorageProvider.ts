eb6cfca7dac348bfc4e81b5003546aa70261b7df
ggonzalez <ggonzalez@tatico.co>
2020-09-26 15:52:53 -0300

    Amazon S3

---
+++ backend/src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import path from 'path';
+import aws from 'aws-sdk';
+import mime from 'mime';
+
+import uploadConfig from '@config/upload';
+import IStorageProvider from "../models/IStorageProvider";
+import AppError from '@shared/errors/AppError';
+
+class DiskStorageProvider implements IStorageProvider {
+  private client: aws.S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      region: 'us-east-1'
+    })
+  }
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const contentType = mime.getType(originalPath);
+
+    if (!contentType)
+      throw new AppError('File not found.');
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    await this.client.putObject({
+      Bucket: uploadConfig.config.aws.bucket,
+      Key: file,
+      ACL: 'public-read',
+      Body: fileContent,
+      ContentType: contentType,
+    }).promise();
+
+    await fs.promises.unlink(originalPath);
+
+    return file;
+  }
+  public async deleteFile(file: string): Promise<void> {
+    await this.client.deleteObject({
+      Bucket: uploadConfig.config.aws.bucket,
+      Key: file
+    }).promise();
+  }
+}
+
+export default DiskStorageProvider;
